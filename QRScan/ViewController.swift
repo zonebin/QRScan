21b40e6019338eaf7df65ef5a4b4edf6182f84e6
张朝辉 <zonebin2008@gmail.com>
2016-07-25 13:11:55 +0800

    Initial Commit

---
+++ QRScan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRScan
+//
+//  Created by 张朝辉 on 16/7/25.
+//  Copyright © 2016年 zonebin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
